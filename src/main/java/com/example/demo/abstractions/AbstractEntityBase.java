6a1fa1876b6d11d4c904918c6977f635194a5eba
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-06-16 21:59:12 +0200

    feat: changes in hashCode and equals

--- src/main/java/com/example/demo/abstractions/AbstractEntityBase.java
+++ src/main/java/com/example/demo/abstractions/AbstractEntityBase.java
@@ -5,10 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.Instant;
+import java.util.Objects;
 
 // Just for demo, it is not needed in this app
+// https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+// https://jpa-buddy.com/blog/hopefully-the-final-article-about-equals-and-hashcode-for-jpa-entities-with-db-generated-ids/
 @Getter
 @Setter
 @NoArgsConstructor
@@ -36,4 +40,20 @@ public abstract class AbstractEntityBase <T extends AbstractEntityBase<T>> {
     }
 
     public abstract void update(T entity);
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        AbstractEntityBase<?> other = (AbstractEntityBase<?>) o;
+        return getId() != null && Objects.equals(getId(), other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/com/example/demo/models/Movie.java
+++ src/main/java/com/example/demo/models/Movie.java
@@ -5,9 +5,6 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
-
-import java.util.Objects;
 
 @Entity
 @Getter
@@ -29,21 +26,5 @@ public class Movie extends AbstractEntityBase<Movie> {
         if (movie.director != null) {
             director = movie.director;
         }
-    }
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Movie movie = (Movie) o;
-        return getId() != null && Objects.equals(getId(), movie.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
